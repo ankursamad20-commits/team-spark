6a645938bd0aa55867b2a9815664bcd25759884d
ankursamad20-commits <ankursamad20@gmail.com>
2025-08-25 08:42:35 +0530

    Create variables.js

---
+++ day1/variables.js
@@ -0,0 +1,9 @@
+// Learning variables and types
+
+let college = "SISTec Bhopal";  
+let batch = 2027;  
+let isCoder = true;  
+
+console.log("College:", college);
+console.log("Batch:", batch);
+console.log("Coder:", isCoder);
