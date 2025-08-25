11922b44ff3f856f1a8837f15406012266c5429d
ankursamad20-commits <ankursamad20@gmail.com>
2025-08-25 08:43:11 +0530

    Create students.js

---
+++ day1/students.js
@@ -0,0 +1,17 @@
+// student.js
+// Mini task: Print student details from an object
+
+const student = {
+    name: "Vipin Tomar",
+    age: 19,
+    course: "B.Tech CSE (AI & DS)",
+    college: "SISTec Bhopal",
+    club: "Kaggle Koders"
+};
+
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("College:", student.college);
+console.log("Club:", student.club);
