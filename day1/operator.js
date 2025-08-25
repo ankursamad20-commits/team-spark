fdd0d6898a17a6a34a30f5ac7d26013ef08499e7
ankursamad20-commits <ankursamad20@gmail.com>
2025-08-25 08:41:50 +0530

    Create operator.js

---
+++ day1/operator.js
@@ -0,0 +1,11 @@
+// operators.js
+// Learning operators
+
+let a = 15, b = 5;
+
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a > b?", a > b);
